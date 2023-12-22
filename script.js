1e56adc271b2261ba917b9d530026fb5023f140c
erenozy <ozy.066@outlook.com>
2023-12-22 20:24:33 +0100

    Added Script

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function submitButton() {
+
+let oneFirst = document.getElementById("nameinput").value;
+let twoSecond = document.getElementById("datebutton").value;
+let threeThird = document.getElementById("amountbutton").value;
+
+
+console.log([oneFirst], [twoSecond] , [threeThird]);
+
+}
